6b8401cac18d65932c7474b0214f584e46eb9cfe
AleniaReyo <119845638+reyo-dev@users.noreply.github.com>
2026-09-15 14:26:51 +0200

    Correcting message methods' calls

--- endings.py
+++ endings.py
@@ -1,5 +1,7 @@
 import pygame
-import message
+from UI import UI
+from message import Message
+
 
 class Endings:
     score: int = 0
@@ -8,40 +10,40 @@ class Endings:
         self.score = score
 
     def main(self):
-        message_end = Message(16)
-        message_credits = Message(32)
+        message_end = Message(UI.screen)
+        message_credits = Message(UI.screen)
         for event in pygame.event.get():
-            if event.type == gameEnd:
+            if event.type == game_end:
                 if self.score < 0:
-                    message_end.print("Patron : Mais que faites-vous?! Je ne vous paie pas pour cela !")
-                    message_end.print("Patron : Vous êtes renvoyé·e !")
-                    message_end.print("VOUS AVEZ ÉTÉ RENVOYÉ·E. TOUT LE MONDE EST MORT.")
-                elif self.score < 10000000:
-                    message_end.print("VOTRE FUSÉE N'A PAS DÉCOLLÉ.")
-                    message_end.print("L'ÉCLIPSE A EU LIEU. RIEN N'EST ARRIVÉ. LE MONDE EST SAUVÉ")
+                    message_end.show("Patron : Mais que faites-vous?! Je ne vous paie pas pour cela !", "Suivant")
+                    message_end.show("Patron : Vous êtes renvoyé·e !", "Suivant")
+                    message_end.show("VOUS AVEZ ÉTÉ RENVOYÉ·E. TOUT LE MONDE EST MORT.", "Fin")
+                elif self.score < 1:
+                    message_end.show("VOTRE FUSÉE N'A PAS DÉCOLLÉ.\
+                    L'ÉCLIPSE A EU LIEU. RIEN N'EST ARRIVÉ. LE MONDE EST SAUVÉ", "Fin")
                 elif self.score < 410000000:
-                    message_end.print("VOTRE FUSÉE A DÉCOLLÉ. ELLE S'EST MALHEUREUSEMENT ÉCRASÉE, PAR MANQUE DE CARBURANT.")
-                    message_end.print("LA VILLE DE WASHINGTON D.C. A ÉTÉ RASÉE.")
-                    message_end.print("DE NOUVELLES ÉLECTIONS ONT ÉTÉ ORGANISÉES. SNOOP DOGG EST ÉLU AVEC 67% DES VOIX.")
+                    message_end.show("VOTRE FUSÉE A DÉCOLLÉ. ELLE S'EST MALHEUREUSEMENT ÉCRASÉE, PAR MANQUE DE CARBURANT.\
+                    LA VILLE DE WASHINGTON D.C. A ÉTÉ RASÉE.\
+                    DE NOUVELLES ÉLECTIONS ONT ÉTÉ ORGANISÉES. SNOOP DOGG EST ÉLU AVEC 67% DES VOIX.", "Fin")
                 elif self.score < 430000000:
-                    message_end.print("VOTRE FUSÉE A DÉCOLLÉ. ELLE A DÉVIÉ DE SA TRAJECTOIRE, S'EST ARRÊTÉE, ET SE PERD DANS L'ESPACE.")
-                    message_end.print("L'ÉCLIPSE A EU LIEU. RIEN N'EST ARRIVÉ. LE MONDE EST SAUVÉ.")
+                    message_end.show("VOTRE FUSÉE A DÉCOLLÉ. ELLE A DÉVIÉ DE SA TRAJECTOIRE, S'EST ARRÊTÉE, ET SE PERD DANS L'ESPACE.\
+                    L'ÉCLIPSE A EU LIEU. RIEN N'EST ARRIVÉ. LE MONDE EST SAUVÉ.", "Fin")
                 elif self.score < 750000000:
-                    message_end.print("VOTRE FUSÉE A DÉCOLLÉ. ELLE A DÉVIÉ DE SA TRAJECTOIRE ET S'EST ARRÊTÉE EN ORBITE LUNAIRE.")
-                    message_end.print("DES HABITANTS DE LA LUNE ONT FAIT REPARTIR LA FUSÉE VERS LA TERRE.")
-                    message_end.print("TOUT LE MONDE EST MORT.")
+                    message_end.show("VOTRE FUSÉE A DÉCOLLÉ. ELLE A DÉVIÉ DE SA TRAJECTOIRE ET S'EST ARRÊTÉE EN ORBITE LUNAIRE.\
+                    DES HABITANTS DE LA LUNE ONT FAIT REPARTIR LA FUSÉE VERS LA TERRE.\
+                    TOUT LE MONDE EST MORT.", "Fin")
                 elif self.score < 999999999:
-                    message_end.print("VOTRE FUSÉE A DÉCOLLÉ. ELLE A LÉGÈREMENT DÉVIÉ DE SA TRAJECTOIRE ET SE DIRIGE VERS LE SOLEIL.")
-                    message_end.print("LE SOLEIL EXPLOSE")
-                    message_end.print("8 MINUTES PLUS TARD, TOUT LE MONDE EST MORT.")
+                    message_end.show("VOTRE FUSÉE A DÉCOLLÉ. ELLE A LÉGÈREMENT DÉVIÉ DE SA TRAJECTOIRE ET SE DIRIGE VERS LE SOLEIL.\
+                    LE SOLEIL EXPLOSE.\
+                    8 MINUTES PLUS TARD, TOUT LE MONDE EST MORT.", "Fin")
                 elif self.score == 1000000000:
-                    message_end.print("VOTRE FUSÉE A DÉCOLLÉ. ELLE A ATTEINT SA CIBLE. LA LUNE EXPLOSE.")
-                    message_end.print("LES DÉBRIS DE LA LUNE RETOMBENT SUR LA TERRE.")
-                    message_end.print("TOUT LE MONDE EST MORT.")
+                    message_end.show("VOTRE FUSÉE A DÉCOLLÉ. ELLE A ATTEINT SA CIBLE. LA LUNE EXPLOSE.\
+                    LES DÉBRIS DE LA LUNE RETOMBENT SUR LA TERRE.\
+                    TOUT LE MONDE EST MORT.", "Fin")
                 else:
-                    message_end.print("undefined Fin")
+                    message_end.show("undefined Fin", "Undefined")
             break
-        message_credits.print("Jeu réalisé dans le cadre de la SAE5.01: GameJam, du BUT Informatique, à l'université Grenoble-Alpes.\
+        message_credits.show("Jeu réalisé dans le cadre de la SAE5.01: GameJam, du BUT Informatique, à l'université Grenoble-Alpes.\
                                 Création : équipe des croustiflambs (le b est muet)\
                                 Programmation : Célia MOULIN, Alenia LEFOYER, Yvan GIORDANO, Timothée DAGAND\
                                 Assets graphiques : Emma DHOURY\
@@ -50,4 +52,4 @@ class Endings:
                                 Merci à Jean-Pierre CHEVALLET pour son cours sur le langage Python et ses conseils lors du développement,\
                                 Merci à l'équipe enseignante du BUT Informatique de l'université Grenoble-Alpes,\
                                 Enfin, merci à vous d'avoir joué !\
-        ")
+        ", "Quitter")
