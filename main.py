25495ead4fc3a39942284aba2973c6d595b969e2
Yvan Giordano <yvan.giordano@gmail.com>
2026-09-15 08:41:31 +0200

    intégration ui

--- main.py
+++ main.py
@@ -6,28 +6,39 @@ def main():
     # Démmarre le module
     pygame.init()
     # définit l'écran et sa taille, retourne la surface pour le dessin
-    screen = pygame.display.set_mode((640, 360))
+    screen = pygame.display.set_mode((640, 360), pygame.FULLSCREEN | pygame.SCALED)
     # Pour savoir quand la boucle du jeu se termine
     running = True
 
-    font = pygame.font.SysFont(None, 80)
-    nb_click = 0
+    # Image de fond
+    background_image = pygame.image.load("assets/Background.png")
+
+    # Image écran compteur
+    screen_score_image = pygame.image.load("assets/Screen_Score.png")
+
+    # Images bouton
+    button_up_image = pygame.image.load("assets/Button_Up.png")
+    button_down_image = pygame.image.load("assets/Button_Down.png")
+
+    # Police d'écriture nb_click
+    font = pygame.font.Font("assets/EarlyGameBoy.ttf", 16)
+    score = 100
+
+    # Bouton
+    button_rect = pygame.Rect(296, 220, 48, 48)
+
+    # Affichage initial
+    screen.blit(background_image, (0, 0))
+    screen.blit(button_up_image, (296, 220))
 
     # Boucle de l'animation
     while running:
-        # Couleur de fond
-        screen.fill("gray")
-
-        # Texte
-        text = font.render(f"{nb_click}", True, (0, 0, 0))
-        text_rect = text.get_rect()
-        text_rect.center = ((int)(screen.get_width() / 2), (int)(screen.get_height() / 2 - 100))
-        screen.blit(text, text_rect)
-
-        # Bouton
-        button_rect = pygame.Rect(screen.get_width() / 2 - 50, screen.get_height() / 2 - 50, 100, 100)
-        pygame.draw.rect(screen, "red", button_rect)
-        pygame.display.flip()
+        # Texte score
+        screen.blit(screen_score_image, (282, 185))
+        score_text = font.render(f"{score}", True, (255, 255, 255))
+        score_text_rect = score_text.get_rect()
+        score_text_rect.center = (319, 194)
+        screen.blit(score_text, score_text_rect)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -37,7 +48,10 @@ def main():
                 # Incrémente lors d'un clic sur le bouton
                 if (button_rect.left < pygame.mouse.get_pos()[0] < button_rect.right
                         and button_rect.top < pygame.mouse.get_pos()[1] < button_rect.bottom):
-                    nb_click += 1
+                    score += 1
+                    screen.blit(button_down_image, (296, 220))
+            elif event.type == pygame.MOUSEBUTTONUP:
+                screen.blit(button_up_image, (296, 220))
 
         # Quitter le jeu
         keys = pygame.key.get_pressed()
