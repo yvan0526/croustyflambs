1f71fbc9bc97ba3a823f035a420b9fa5917cfa48
AleniaReyo <119845638+reyo-dev@users.noreply.github.com>
2026-09-15 15:26:01 +0200

    Redo ending handler

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import math
 import pygame
 from UI import UI
 from message import Message
+from events import GAME_END
+from events import WINDOW_BREAK
 
 from src.modele.etat import Etat
 
@@ -39,6 +41,10 @@ def main():
         dt = clock.tick(60)
         t += dt
 
+        # Fin réservoir plein
+        if etat.score >= 1000000000000000:
+            pygame.event.post(GAME_END)
+
         # Lune
         ui.display_window(moon_angle)
         if moon_angle < 0:
@@ -60,6 +66,53 @@ def main():
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == WINDOW_BREAK:
+                etat.score = -1
+                pygame.event.post(GAME_END)
+
+            if event.type == GAME_END:
+                message_end = Message(UI.screen)
+                message_credits = Message(UI.screen)
+                if etat.score < 0: # Casser la fenêtre met le score à une valeur négative et termine le jeu
+                    message_end.show("Patron : Mais que faites-vous?! Je ne vous paie pas pour cela !", "Suivant")
+                    message_end.show("Patron : Vous êtes renvoyé·e !", "Suivant")
+                    message_end.show("VOUS AVEZ ÉTÉ RENVOYÉ·E. TOUT LE MONDE EST MORT.", "Fin")
+                elif etat.score < 1:
+                    message_end.show("VOTRE FUSÉE N'A PAS DÉCOLLÉ.\
+                    L'ÉCLIPSE A EU LIEU. RIEN N'EST ARRIVÉ. LE MONDE EST SAUVÉ", "Fin")
+                elif etat.score < 410000000000000:
+                    message_end.show("VOTRE FUSÉE A DÉCOLLÉ. ELLE S'EST MALHEUREUSEMENT ÉCRASÉE, PAR MANQUE DE CARBURANT.\
+                    L'IUT2 DE GRENOBLE A ÉTÉ RASÉ.\
+                    CÉDRIC GÉROT, S'ÉTANT RECONVERTI, EST ÉLU PRÉSIDENT DE LA RÉPUBLIQUE FRANÇAISE AVEC 69% DES VOIX.", "Fin")
+                elif etat.score < 430000000000000:
+                    message_end.show("VOTRE FUSÉE A DÉCOLLÉ. ELLE A DÉVIÉ DE SA TRAJECTOIRE, S'EST ARRÊTÉE, ET SE PERD DANS L'ESPACE.\
+                    L'ÉCLIPSE A EU LIEU. RIEN N'EST ARRIVÉ. LE MONDE EST SAUVÉ.", "Fin")
+                elif etat.score < 750000000000000:
+                    message_end.show("VOTRE FUSÉE A DÉCOLLÉ. ELLE A DÉVIÉ DE SA TRAJECTOIRE ET S'EST ARRÊTÉE EN ORBITE LUNAIRE.\
+                    DES HABITANTS DE LA LUNE ONT FAIT REPARTIR LA FUSÉE VERS LA TERRE.\
+                    LA POPULATION TERRESTRE EST RÉDUITE EN ESCLAVAGE.", "Fin")
+                elif etat.score < 999999999999999:
+                    message_end.show("VOTRE FUSÉE A DÉCOLLÉ. ELLE A LÉGÈREMENT DÉVIÉ DE SA TRAJECTOIRE ET SE DIRIGE VERS LE SOLEIL.\
+                    LE SOLEIL EXPLOSE.\
+                    8 MINUTES PLUS TARD, TOUT LE MONDE EST MORT.", "Fin")
+                elif etat.score == 1000000000000000:
+                    message_end.show("VOTRE FUSÉE A DÉCOLLÉ. ELLE A ATTEINT SA CIBLE. LA LUNE EXPLOSE.\
+                    LES DÉBRIS DE LA LUNE RETOMBENT SUR LA TERRE.\
+                    TOUT LE MONDE EST MORT.", "Fin")
+                else:
+                    message_end.show("undefined Fin", "Undefined")
+                message_credits.show("Jeu réalisé dans le cadre de la SAE5.01: GameJam, du BUT Informatique, à l'université Grenoble-Alpes.\
+                                                Création : équipe des croustiflambs (le b est muet)\
+                                                Programmation : Célia MOULIN, Alenia LEFOYER, Yvan GIORDANO, Timothée DAGAND\
+                                                Assets graphiques : Emma DHOURY\
+                                                Sons : \
+                                                Remerciements :\
+                                                Merci à Jean-Pierre CHEVALLET pour son cours sur le langage Python et ses conseils lors du développement,\
+                                                Merci à l'équipe enseignante du BUT Informatique de l'université Grenoble-Alpes,\
+                                                Enfin, merci à vous d'avoir joué !", "Quitter")
                 pygame.quit()
                 quit()
 
