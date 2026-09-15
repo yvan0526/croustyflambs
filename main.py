e15bfe40aa9388b79e58a2c1df670c6f2ebcb9fd
Célia Moulin <celia.moulin@free.fr>
2026-09-15 13:47:15 +0200

    Message

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import math
 
 import pygame
-
 from UI import UI
-
+from message import Message
 
 def main():
-    # Démmarre le module
+    # Démarre le module
     pygame.init()
 
     ui = UI()
+
+    message = Message(ui.screen)
+    # Message affiché au démarrage
+    message.show("MESSAGE D’URGENCE\n"
+                 "Vous êtes notre seul espoir.\n"
+                 "Une éclipse aura lieu dans 10 minutes. Si elle se produit, ce sera la fin du monde.\n"
+                 "Votre mission est simple : remplir entièrement le réservoir de la fusée afin de la lancer et de détruire la Lune avant le debut de l’éclipse.\n"
+                 "Chaque seconde compte. Chaque clic peut faire la différence.\n"
+                 "Ne nous décevez pas.\n", "Ok")
+
     running = True
     score = 100
 
@@ -22,6 +31,11 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+
+            if message.active:
+                message.handle_event(event)
+                continue
+
             elif event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
                 # Incrémente lors d'un clic sur le bouton
                 if ui.check_mouse_click_fuelle_button():
@@ -36,6 +50,7 @@ def main():
             running = False
 
         # Mise à jour de l'affichage
+        message.draw()
         pygame.display.update()
 
 if __name__ == '__main__':
