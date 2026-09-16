8f26d5222e6d986dbda52bfe8eab33688ca21acb
arowana755 <silure.38@gmail.com>
2026-09-16 09:59:26 +0200

    Add methods checking whether you can upgrade or not

--- src/modele/etat.py
+++ src/modele/etat.py
@@ -25,38 +25,53 @@ class Etat:
         self.score += self.autocliqueur.valeur
         self.autocliqueur.nb_tot_clics += 1
 
-    # Méthode gérant l'amélioration du clic droit en vérifiant la possibilité de l'acheter
+    # Méthode gérant l'amélioration du clic droit
     def debloque_clic_droit(self)-> None :
-        if not self.clic_droit_debloque and self.score >= 1000:
+        if self.peut_debloquer_clic_droit():
             self.score -= 1000
             self.clic_droit_debloque = True
+    # Vérifie la possibilité d'améliorer le clic droit
+    def peut_debloquer_clic_droit(self)-> bool :
+        return not self.clic_droit_debloque and self.score >= 1000
 
-    # Méthode gérant l'amélioration de la valeur d'un clic en vérifiant la possibilité de l'acheter
+    # Méthode gérant l'amélioration de la valeur d'un clic
     def add_valeur_clic(self, bonus: int = 1)-> None :
-        if self.valeur_clic <= 10 and self.score >= self.PRIX_AMELIORATION[self.nb_ameliorations]:
+        if self.peut_add_valeur_clic():
             self.valeur_clic += bonus
             self.score -= self.PRIX_AMELIORATION[self.nb_ameliorations]
             self.nb_ameliorations += 1
+    # Vérifie la possibilité d'améliorer la valeur du clic
+    def peut_add_valeur_clic(self)-> bool :
+        return self.valeur_clic <= 10 and self.score >= self.PRIX_AMELIORATION[self.nb_ameliorations]
 
-    # Méthode gérant l'amélioration de la valeur de l'autoclic en vérifiant la possibilité de l'acheter
+    # Méthode gérant l'amélioration de la valeur de l'autoclic
     def add_autoclic_val(self, bonus: int = 1)-> None :
-        if self.autocliqueur.valeur <= 10 and self.score >= self.PRIX_AMELIORATION[self.nb_ameliorations]:
+        if self.peut_add_autoclic_val():
             self.autocliqueur.valeur += bonus
             self.score -= self.PRIX_AMELIORATION[self.nb_ameliorations]
             self.nb_ameliorations += 1
+    # Vérifie la possibilité d'améliorer la valeur dde l'autoclic
+    def peut_add_autoclic_val(self)-> bool :
+        return self.autocliqueur.valeur <= 10 and self.score >= self.PRIX_AMELIORATION[self.nb_ameliorations]
 
-    # Méthode gérant l'amélioration de la fréquence de l'autoclic en vérifiant la possibilité de l'acheter
+    # Méthode gérant l'amélioration de la fréquence de l'autoclic
     def add_autoclic_cps(self, bonus: int = 1)-> None :
-        if self.autocliqueur.cps <= 15 and self.score >= self.PRIX_AMELIORATION[self.nb_ameliorations]:
+        if self.peut_add_autoclic_cps():
             self.autocliqueur.cps += bonus
             self.score -= self.PRIX_AMELIORATION[self.nb_ameliorations]
             self.nb_ameliorations += 1
+    # Vérifie la possibilité d'améliorer la fréquence de l'autoclic
+    def peut_add_autoclic_cps(self)-> bool :
+        return self.autocliqueur.cps <= 15 and self.score >= self.PRIX_AMELIORATION[self.nb_ameliorations]
 
-    # Méthode gérant l'amélioration du nombre d'autocliqueurs en vérifiant la possibilité de l'acheter
+    # Méthode gérant l'amélioration du nombre d'autocliqueurs
     def add_autocliqueur(self, t: int, bonus: int = 1)-> None :
-        if self.autocliqueur.quantite <= 10 and self.score >= self.PRIX_AMELIORATION[self.nb_ameliorations]:
+        if self.peut_add_autocliqueur():
             self.autocliqueur.quantite += bonus
             self.score -= self.PRIX_AMELIORATION[self.nb_ameliorations]
             self.nb_ameliorations += 1
             if self.nb_ameliorations == 0:
                 self.autocliqueur.temps_premier = t
+    # Vérifie la possibilité d'améliorer le nombre d'autocliqueurs
+    def peut_add_autocliqueur(self)-> bool :
+        return self.autocliqueur.quantite <= 10 and self.score >= self.PRIX_AMELIORATION[self.nb_ameliorations]
