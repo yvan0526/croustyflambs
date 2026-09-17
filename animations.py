6980446781974d3247a84bab2d552be4cff2d5ef
Yvan Giordano <yvan.giordano@gmail.com>
2026-09-17 15:51:33 +0200

    fix fins

--- animations.py
+++ animations.py
@@ -14,9 +14,9 @@ def get_end_animation(ui, score):
         return Animation(ui.screen, ui.end_happy_images, 220, 20)
     elif percent < 100:
         return Animation(ui.screen, ui.end_luniens_images, 220, 20)
-    elif percent <= 105:
+    elif score <= Etat.SCORE_GOAL * 1.05:
         return Animation(ui.screen, ui.end_100_images, 220, 20)
-    elif percent > 105:
+    elif score > Etat.SCORE_GOAL * 1.05:
         return Animation(ui.screen, ui.end_sun_images, 220, 20)
     else:
         return Animation(ui.screen, ui.end_iut_images, 220, 20)
