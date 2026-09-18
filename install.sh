05669c3e3ffad6886099c0199365cef8efb7b9bd
AleniaReyo <119845638+reyo-dev@users.noreply.github.com>
2026-09-18 15:52:49 +0200

    fix paths

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 python3 -m venv ./venv
-.venv/bin/pip install -r requirements.txt
-.venv/bin/pip freeze > requirements.txt
+./venv/bin/pip install -r requirements.txt
+./venv/bin/pip freeze > requirements.txt
 ln YouHave10MinutesToSaveTheWorldBeforeTheEclipseOrEveryoneWillDIE "$HOME/Desktop/"
